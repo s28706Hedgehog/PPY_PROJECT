50ee04655960942767308cb668743a73b5f9f0e5
Undead Hedgehog <undeadhedgehohgmc@gmail.com>
2025-06-10 04:41:03 +0200

    Improve launcher clarity

--- src/launcher.py
+++ src/launcher.py
@@ -1,13 +1,7 @@
 from src.loader import JsonTaskLoader
 from src.menu.MenuRefactor import ConsoleWindowManager, MainConsoleWindow
 
-# TODO: Log system
-
 tasks = JsonTaskLoader.load_all_tasks(r"C:\Users\jerzy\Desktop\TaskManager\rsc")
-# menu = Menu(tasks)
-# menu.show_menu(menu.main_options, menu.main_actions)
-
-# ConsoleMenu.show_menu()
 
 print("Tasks" + str(tasks))
 window_manager = ConsoleWindowManager(tasks)
@@ -15,22 +9,15 @@ window_manager.add_new_window(MainConsoleWindow(tasks))
 window_manager.show_current_window()
 
 """
-myTask = Task.create_unfinished_task(
-    'Simple Task',
-    TaskPriority.URGENT_IMPORTANT,
-    TaskCategory.PERSONAL,
-    'Simple description',
-    datetime.now(),
-    "python print('anything')")
-
-myTask.start_task()
-"""
-
-"""
-result = json.dumps(myTask.to_dict())
-print(result)
-
-simpleFile = open("simpleFile.json", 'w')
-simpleFile.write(result)
-simpleFile.close()
+    1. Adding new tasks ( both loading from files and hand written )
+    2. Removing / editing Tasks
+    3. Marking tasks as finished
+    4. Print all task data ( specified task? )
+    5. Print list of tasks ( all data but without description )
+    6. Sort tasks by: name, priority, due date ( asc / desc ), state ( asc / desc )
+    7. Filter tasks: task_priority, due_date(today, this week, this month), category, task_state
+    8. Save / loading to file
+    9. Generating statistics with charts ( average working time, % of tasks done on time, most frequent category )
+    10. Handling exceptions ( minimum 10 )
+    11. Simple interface
 """
