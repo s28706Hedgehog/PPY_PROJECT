1132dfdb244945960ca56ef8ee1cc5cdc3cac95c
Jerzy <s28706@pjwstk.edu.pl>
2025-06-11 17:27:22 +0200

    TODO list inside launcher module

--- src/launcher.py
+++ src/launcher.py
@@ -11,3 +11,17 @@ print("Tasks" + str(tasks))
 window_manager = ConsoleWindowManager(tasks)
 window_manager.add_new_window(MainConsoleWindow(tasks, MenuSettings()))
 window_manager.show_current_window()
+
+"""
+TODO: ( Fixes, improvements, new features )
+    1) Cleanup MenuRefactor module
+    2) Handle exceptions properly
+    3) Finish settings / options / filter
+    4) Cleanup exceptions
+    5) Optimise imports
+    6) Check for return's of functions type annotations
+    7) Fix task not being able to terminate
+    8) Deadline validation ( or any kind of info, like FINISHED_AFTER_DEADLINE )
+    9) Separate module for handling paths
+    10) Small tweaks to display
+"""
